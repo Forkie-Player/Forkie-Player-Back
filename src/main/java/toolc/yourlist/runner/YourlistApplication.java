bf4dd0a83cfa3785e45e5ce4cd4e6f28fc8a8174
oh980225 <oh980225@gmail.com>
2021-11-11 14:44:56 +0900

    config: runner패키지로 YourlistApplication 이동

---
+++ src/main/java/toolc/yourlist/runner/YourlistApplication.java
@@ -0,0 +1,15 @@
+package toolc.yourlist.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "toolc.yourlist")
+@EnableJpaRepositories(basePackages = "toolc.yourlist")
+@EntityScan(basePackages = "toolc.yourlist")
+public class YourlistApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(YourlistApplication.class, args);
+	}
+}
