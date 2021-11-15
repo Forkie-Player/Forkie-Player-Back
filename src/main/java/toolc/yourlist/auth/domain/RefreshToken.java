e961dc919ea8095b93cb3ba000ce7454c701514d
JisooPark27 <jspark9812@gmail.com>
2021-11-15 18:25:04 +0900

    feat : RefreshToken - equals

--- src/main/java/toolc/yourlist/auth/domain/RefreshToken.java
+++ src/main/java/toolc/yourlist/auth/domain/RefreshToken.java
@@ -2,6 +2,13 @@ package toolc.yourlist.auth.domain;
 
 import lombok.EqualsAndHashCode;
 
+import java.time.Instant;
+
 @EqualsAndHashCode
 public class RefreshToken {
+  private final Instant expirationAt;
+
+  public RefreshToken(Instant expirationAt) {
+    this.expirationAt = expirationAt;
+  }
 }
--- src/test/java/toolc/yourlist/auth/domain/RefreshTokenTest.java
+++ src/test/java/toolc/yourlist/auth/domain/RefreshTokenTest.java
@@ -2,12 +2,16 @@ package toolc.yourlist.auth.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Instant;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 class RefreshTokenTest {
   @Test
   void equals() {
-    assertThat(new RefreshToken(), is(new RefreshToken()));
+    Instant expirationTime = Instant.now();
+
+    assertThat(new RefreshToken(expirationTime), is(new RefreshToken(expirationTime)));
   }
 }
