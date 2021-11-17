2eb9fbde8a1a27b39a17ffcfbc175578f000e2c2
JisooPark27 <jspark9812@gmail.com>
2021-11-18 00:36:55 +0900

    fix : AuthApi login request token(accesstoken, refreshtoken)

--- src/main/java/toolc/yourlist/auth/infra/AuthApi.java
+++ src/main/java/toolc/yourlist/auth/infra/AuthApi.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import toolc.yourlist.auth.domain.MemberLogin;
-import toolc.yourlist.auth.domain.Token;
 import toolc.yourlist.common.ResponseBody;
+
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -16,13 +17,15 @@ import static org.springframework.http.HttpStatus.OK;
 public class AuthApi {
 
   private final LoginRequestMapperFromJson loginRequestMapperFromJson;
+  private final TokenFormatter tokenFormatter;
   private final MemberLogin memberLogin;
 
   @PostMapping("/api/login")
   public ResponseEntity<?> login(@RequestBody JsonLoginRequest request) {
-    Token loginToken = memberLogin.login(loginRequestMapperFromJson.mapper(request));
+    Map<String, String> token =
+      tokenFormatter.toJwtFromToken(memberLogin.login(loginRequestMapperFromJson.mapper(request)));
 
-    ResponseBody responseBody = new ResponseBody(OK.value(), "로그인 성공", loginToken);
+    ResponseBody responseBody = new ResponseBody(OK.value(), "로그인 성공", token);
     return ResponseEntity.ok(responseBody);
   }
 }
