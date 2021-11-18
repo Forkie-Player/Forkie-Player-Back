218108c1741bfd89313993cc1aa9736fa802fe9a
JisooPark27 <jspark9812@gmail.com>
2021-11-18 21:02:54 +0900

    feat : NonMember - shold not null and not empty

--- src/main/java/toolc/yourlist/auth/domain/NonMember.java
+++ src/main/java/toolc/yourlist/auth/domain/NonMember.java
@@ -7,6 +7,7 @@ final class NonMember {
   private String uuid;
 
   public NonMember(String uuid) {
+    if(uuid == null || uuid.equals("")) throw new IllegalArgumentException("잘못된 UUID 입니다.");
     this.uuid = uuid;
   }
 }
--- src/test/java/toolc/yourlist/auth/domain/NonMemberTest.java
+++ src/test/java/toolc/yourlist/auth/domain/NonMemberTest.java
@@ -4,11 +4,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NonMemberTest {
 
   @Test
   void equals() {
     assertThat(new NonMember("UUID_abcdefghijkl"), is(new NonMember("UUID_abcdefghijkl")));
+  }
+
+  @Test
+  void non_null() {
+    assertThrows(IllegalArgumentException.class, () ->
+      new NonMember(null));
+  }
+
+  @Test
+  void non_empty() {
+    assertThrows(IllegalArgumentException.class, () ->
+      new NonMember(""));
   }
 }
