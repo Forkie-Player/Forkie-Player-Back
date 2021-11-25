2ce4d3fe525cc231132cb912567c79286dff577b
JisooPark27 <jspark9812@gmail.com>
2021-11-25 20:57:18 +0900

    feat : Separate AccessTokenTest to make AccessTokenCreator

---
+++ src/test/java/toolc/yourlist/auth/domain/AccessTokenCreatorTest.java
@@ -0,0 +1,22 @@
+package toolc.yourlist.auth.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class AccessTokenCreatorTest {
+
+  @Test
+  void token_validated_30_minutes() {
+    CurrentTime currentTime = new CurrentTime();
+    AccessTokenCreator accessTokenCreator = new AccessTokenCreator(currentTime);
+
+    assertThat(accessTokenCreator.create(new LoginId("jisoo27")),
+      is(new AccessToken(new LoginId("jisoo27"),
+        currentTime.now().plus(Duration.ofMinutes(30)))));
+  }
+
+}
