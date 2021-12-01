faa00bc91b86a8a68f302ff0405bb6d4ba577a60
oh980225 <oh980225@gmail.com>
2021-12-01 17:43:59 +0900

    test: LoginIdMapper 테스트 추가

--- src/main/java/toolc/yourlist/playlist/domain/ReadRequest.java
+++ src/main/java/toolc/yourlist/playlist/domain/ReadRequest.java
@@ -1,8 +1,10 @@
 package toolc.yourlist.playlist.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class ReadRequest {
   private final String loginId;
 
