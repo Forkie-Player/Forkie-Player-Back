ff472c5d831870ad71b2da4befaa037afc7ec7dc
JisooPark27 <jspark9812@gmail.com>
2021-12-02 16:53:00 +0900

    refactor : MemberLogin change dependency to TokenProvider

--- src/main/java/toolc/yourlist/auth/domain/MemberLogin.java
+++ src/main/java/toolc/yourlist/auth/domain/MemberLogin.java
@@ -8,16 +8,14 @@ import static io.vavr.control.Either.*;
 @RequiredArgsConstructor
 public class MemberLogin {
   private final AllMember allMember;
-  private final AccessTokenCreator accessTokenCreator;
-  private final RefreshTokenCreator refreshTokenCreator;
+  private final TokenProvider tokenProvider;
   private final CheckPassword checkPassword;
 
   public Either<String, Token> login(LoginRequest request) {
     Member savedMember = allMember.findByLoginId(request.loginId().raw());
 
     if (checkPassword.check(request.password(), savedMember)) {
-      return right(new Token(accessTokenCreator.create(request.loginId()),
-        refreshTokenCreator.create(request.device())));
+      return right(tokenProvider.create(request));
     }
     return left("wrong password");
   }
