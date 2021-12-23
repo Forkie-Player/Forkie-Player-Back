c3be5bfe1bf5a7c6e529b4b499795bb6807fec1e
oh980225 <oh980225@gmail.com>
2021-12-23 16:10:37 +0900

    feat: JSON parse error 처리

--- src/main/java/toolc/yourlist/common/domain/CommonExceptionHandler.java
+++ src/main/java/toolc/yourlist/common/domain/CommonExceptionHandler.java
@@ -1,13 +1,11 @@
 package toolc.yourlist.common.domain;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import toolc.yourlist.common.infra.ResponseBody;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static toolc.yourlist.common.infra.JsonResponse.fail;
 import static toolc.yourlist.common.infra.JsonResponse.failForBadRequest;
 
@@ -16,7 +14,8 @@ public class CommonExceptionHandler {
   @ExceptionHandler(
     value = {
       ContractViolationException.class,
-      MethodArgumentNotValidException.class
+      MethodArgumentNotValidException.class,
+      HttpMessageNotReadableException.class
     }
   )
   public ResponseEntity<?> badRequest(Exception e) {
--- src/main/java/toolc/yourlist/playlist/infra/DeleteApi.java
+++ src/main/java/toolc/yourlist/playlist/infra/DeleteApi.java
@@ -3,6 +3,7 @@ package toolc.yourlist.playlist.infra;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class DeleteApi {
   private final PlaylistEliminator eliminator;
   private final JsonDeleteRequestMapper mapper;
 
-  @PutMapping("/api/playlist")
+  @DeleteMapping("/api/playlist")
   public ResponseEntity<?> delete(@Valid @RequestBody JsonDeleteRequest jsonRequest) {
     var request = mapper.toDeleteRequest(jsonRequest);
     if (request.isEmpty()) {
