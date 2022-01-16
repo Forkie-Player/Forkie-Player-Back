88a94afc18bfa8a6766ce3354dc91f9ffaedf291
oh980225 <oh980225@gmail.com>
2022-01-16 13:07:12 +0900

    fix: http method - POST로 변경

--- src/main/java/toolc/yourlist/playlist/infra/AddPlayApi.java
+++ src/main/java/toolc/yourlist/playlist/infra/AddPlayApi.java
@@ -2,7 +2,7 @@ package toolc.yourlist.playlist.infra;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import toolc.yourlist.playlist.domain.PlayAdder;
@@ -15,7 +15,7 @@ class AddPlayApi {
   private final JsonAddPlayRequestMapper mapper;
   private final PlayAdder adder;
 
-  @GetMapping("/api/play")
+  @PostMapping("/api/play")
   public ResponseEntity<?> addPlay(@RequestBody JsonAddPlayRequest jsonRequest) {
     var request = mapper.toAddPlayRequest(jsonRequest);
 
