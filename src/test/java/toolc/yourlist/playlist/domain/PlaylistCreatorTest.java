dad451c04e6083b958a522fb7c2fa0dd414cdd0d
oh980225 <oh980225@gmail.com>
2022-01-23 15:29:58 +0900

    test: PlaylistCreatorTest -> Mockito 이용해서 오케스트레이션 책임 검증

--- src/test/java/toolc/yourlist/playlist/domain/PlaylistCreatorTest.java
+++ src/test/java/toolc/yourlist/playlist/domain/PlaylistCreatorTest.java
@@ -1,23 +1,56 @@
 package toolc.yourlist.playlist.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static io.vavr.control.Either.left;
+import static io.vavr.control.Either.right;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
-// TODO: Mockito이용해서 오케스트레이션 -> 함수의 콜을 확인하는 테스트 필요
+@ExtendWith(MockitoExtension.class)
 class PlaylistCreatorTest {
   @Test
-  void createPlaylist() {
-    var allPlaylists = new MockAllPlaylists();
-    var allMember = new MockAllMember();
+  void create(@Mock AllMember allMember, @Mock AllPlaylists allPlaylists) {
     var creator = new PlaylistCreator(allMember, allPlaylists);
-    var request = new SaveRequest(
-      1L,
-      "My List");
+    var request = new SaveRequest(1L, "My List");
+    given(allMember.findById(request.memberId())).willReturn(
+      Member.builder()
+        .id(request.memberId())
+        .loginId("oh980225")
+        .password("qwer1234!")
+        .isMember(true)
+        .build());
+    given(allPlaylists.havingCountOf(request.memberId())).willReturn(5L);
 
-    var actual = creator.create(request).get();
+    var actual = creator.create(request);
 
-    assertThat(actual, is(true));
+    assertThat(actual, is(right(Boolean.TRUE)));
+    verify(allMember).findById(request.memberId());
+    verify(allPlaylists).havingCountOf(request.memberId());
+  }
+
+  @Test
+  void create_exceed_limit(@Mock AllMember allMember, @Mock AllPlaylists allPlaylists) {
+    var creator = new PlaylistCreator(allMember, allPlaylists);
+    var request = new SaveRequest(1L, "My List");
+    given(allMember.findById(request.memberId())).willReturn(
+      Member.builder()
+        .id(request.memberId())
+        .loginId("oh980225")
+        .password("qwer1234!")
+        .isMember(false)
+        .build());
+    given(allPlaylists.havingCountOf(request.memberId())).willReturn(5L);
+
+    var actual = creator.create(request);
+
+    assertThat(actual, is(left("비회원의 영상 생성 제한을 넘었습니다.")));
+    verify(allMember).findById(request.memberId());
+    verify(allPlaylists).havingCountOf(request.memberId());
   }
 }
