4e9319106d9d1b877aa36b34140533f76b25b4f4
oh980225 <oh980225@gmail.com>
2022-01-23 15:21:06 +0900

    test: PlaylistEliminatorTest -> Mockito 이용해서 오케스트레이션 책임 검증

--- src/test/java/toolc/yourlist/playlist/domain/PlaylistEliminatorTest.java
+++ src/test/java/toolc/yourlist/playlist/domain/PlaylistEliminatorTest.java
@@ -1,9 +1,36 @@
 package toolc.yourlist.playlist.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-// TODO: Mockito이용해서 오케스트레이션 -> 함수의 콜을 확인하는 테스트 필요
+@ExtendWith(MockitoExtension.class)
 class PlaylistEliminatorTest {
+  @Test
+  void delete(@Mock AllPlaylists allPlaylists) {
+    var eliminator = new PlaylistEliminator(allPlaylists);
+    var request = new DeleteRequest(
+      new EqualOwner(
+        Member.builder()
+          .id(1L)
+          .loginId("oh980225")
+          .password("qwer1234!")
+          .isMember(true)
+          .build(),
+        Playlist.builder()
+          .id(1L)
+          .memberId(1L)
+          .title("My List")
+          .thumbnail("panda.png")
+          .build()));
+
+    eliminator.delete(request);
+
+    verify(allPlaylists).delete(request.equalOwner().playlist());
+    verifyNoMoreInteractions(allPlaylists);
+  }
 }
