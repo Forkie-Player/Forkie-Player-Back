d2db6c14f4e972882548ddbee47d17967c15e071
oh980225 <oh980225@gmail.com>
2022-01-23 15:04:56 +0900

    test: PlaylistUpdaterTest -> Mockito 이용해서 오케스트레이션 책임 검증

--- src/test/java/toolc/yourlist/playlist/domain/PlaylistUpdaterTest.java
+++ src/test/java/toolc/yourlist/playlist/domain/PlaylistUpdaterTest.java
@@ -1,9 +1,40 @@
 package toolc.yourlist.playlist.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-// TODO: Mockito이용해서 오케스트레이션 -> 함수의 콜을 확인하는 테스트 필요
+@ExtendWith(MockitoExtension.class)
 class PlaylistUpdaterTest {
+  @Test
+  void updateTitle(@Mock AllPlaylists allPlaylists) {
+    var updater = new PlaylistUpdater(allPlaylists);
+    var request = new UpdateRequest(
+      new EqualOwner(
+        Member.builder()
+          .id(1L)
+          .loginId("oh980225")
+          .password("qwer1234!")
+          .isMember(true)
+          .build(),
+        Playlist.builder()
+          .id(1L)
+          .memberId(1L)
+          .title("My List")
+          .thumbnail("panda.png")
+          .build()
+      ),
+      "My Music");
+
+    updater.updateTitle(request);
+
+    verify(allPlaylists).updateTitleBelongsTo(
+      request.equalOwner().playlist().id(),
+      request.title());
+    verifyNoMoreInteractions(allPlaylists);
+  }
 }
