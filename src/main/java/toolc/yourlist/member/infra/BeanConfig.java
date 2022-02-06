e0e14c01a01172173d206244de7e4de6b3e040ad
JisooPark27 <jspark9812@gmail.com>
2022-02-06 11:57:01 +0900

    feat : Inject LoginId, password Factory into requsetjsonmapper

--- src/main/java/toolc/yourlist/member/infra/BeanConfig.java
+++ src/main/java/toolc/yourlist/member/infra/BeanConfig.java
@@ -3,10 +3,37 @@ package toolc.yourlist.member.infra;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import toolc.yourlist.member.domain.*;
+import toolc.yourlist.member.domain.loginId.AllLoginPolicy;
+import toolc.yourlist.member.domain.loginId.LoginIdFactory;
+import toolc.yourlist.member.domain.loginId.LoginIdPolicy;
+import toolc.yourlist.member.domain.password.AllPasswordPolicy;
+import toolc.yourlist.member.domain.password.PasswordFactory;
+import toolc.yourlist.member.domain.password.PasswordPolicy;
 import toolc.yourlist.member.infra.jwt.filter.JwtResolver;
 
 @Configuration
 public class BeanConfig {
+
+  @Bean
+  LoginIdPolicy loginIdPolicy() {
+    return new AllLoginPolicy();
+  }
+
+  @Bean
+  LoginIdFactory loginIdFactory(LoginIdPolicy loginIdPolicy) {
+    return new LoginIdFactory(loginIdPolicy);
+  }
+
+  @Bean
+  PasswordPolicy passwordPolicy() {
+    return new AllPasswordPolicy();
+  }
+
+  @Bean
+  PasswordFactory passwordFactory(PasswordPolicy passwordPolicy) {
+    return new PasswordFactory(passwordPolicy);
+  }
+
 
   @Bean
   TokenSecretKey tokenSecretKey() {
@@ -45,8 +72,9 @@ public class BeanConfig {
   }
 
   @Bean
-  RequestMapperFromJson requestMapperFromJson() {
-    return new RequestMapperFromJson();
+  RequestMapperFromJson requestMapperFromJson(LoginIdFactory loginIdFactory,
+                                              PasswordFactory passwordFactory) {
+    return new RequestMapperFromJson(loginIdFactory, passwordFactory);
   }
 
   @Bean
