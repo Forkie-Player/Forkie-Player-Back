c6a81ba54359de193b105453d5612c68199f9dd3
oh980225 <oh980225@gmail.com>
2022-02-10 21:08:40 +0900

    test: test 수정 및 SequenceUpdaterTest의 update메서드 로직 구조 잡기

--- src/main/java/toolc/yourlist/playlist/domain/SequenceUpdater.java
+++ src/main/java/toolc/yourlist/playlist/domain/SequenceUpdater.java
@@ -2,5 +2,9 @@ package toolc.yourlist.playlist.domain;
 
 public class SequenceUpdater {
   public void update(PlaySequencesForUpdate request) {
+    // 1. 입력받은 대로 순서를 변경
+
+    // 2. 1번 순서의 영상의 썹네일을 영상 목록의 썸네일로 변경
+
   }
 }
--- src/test/java/toolc/yourlist/playlist/domain/SequenceUpdaterTest.java
+++ src/test/java/toolc/yourlist/playlist/domain/SequenceUpdaterTest.java
@@ -12,28 +12,53 @@ class SequenceUpdaterTest {
   @Test
   void update() {
     var updater = new SequenceUpdater();
-//    var request = new SequenceUpdateRequest(List.of(new ValidRequestForPlay(
-//      Member.builder()
-//        .id(1L)
-//        .loginId("oh980225")
-//        .password("qwer1234!")
-//        .isMember(true)
-//        .build(),
-//      Playlist.builder()
-//        .id(1L)
-//        .memberId(1L)
-//        .title("My List")
-//        .thumbnail("panda.png")
-//        .build(),
-//      Play.builder()
-//        .id(1L)
-//        .playlistId(1L)
-//        .sequence(1L)
-//        .info(new PlayInfo("So Good Music", "abcd1234", "panda.png"))
-//        .time(new PlayTime(1000L, 3000L))
-//        .channel(new Channel("Music man", "man.png"))
-//        .build())), new PlaySequences());
-//
-//    updater.update(request);
+    var request = new PlaySequencesForUpdate(
+      List.of(
+        new PlaySequence(
+          new ValidRequestForPlay(
+            Member.builder()
+              .id(1L)
+              .loginId("oh980225")
+              .password("qwer1234!")
+              .isMember(true)
+              .build(),
+            Playlist.builder()
+              .id(1L)
+              .memberId(1L)
+              .title("My List")
+              .thumbnail("panda.png")
+              .build(),
+            Play.builder()
+              .id(1L)
+              .playlistId(1L)
+              .sequence(1L)
+              .info(new PlayInfo("So Good Music", "abcd1234", "panda.png"))
+              .time(new PlayTime(1000L, 3000L))
+              .channel(new Channel("Music man", "man.png"))
+              .build()), 2L),
+        new PlaySequence(
+          new ValidRequestForPlay(
+            Member.builder()
+              .id(1L)
+              .loginId("oh980225")
+              .password("qwer1234!")
+              .isMember(true)
+              .build(),
+            Playlist.builder()
+              .id(1L)
+              .memberId(1L)
+              .title("My List")
+              .thumbnail("panda.png")
+              .build(),
+            Play.builder()
+              .id(2L)
+              .playlistId(1L)
+              .sequence(2L)
+              .info(new PlayInfo("So Sad Music", "abcd1234", "puppyu.png"))
+              .time(new PlayTime(2000L, 3000L))
+              .channel(new Channel("Music man", "man.png"))
+              .build()), 1L)));
+
+    updater.update(request);
   }
 }
