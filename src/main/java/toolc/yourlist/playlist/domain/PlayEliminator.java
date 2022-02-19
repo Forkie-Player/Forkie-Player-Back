699a4546e3bedb3b2b0fb5a5c10040fd9967b133
oh980225 <oh980225@gmail.com>
2022-02-19 22:22:22 +0900

    test: PlayEliminatorTest 작성

--- src/main/java/toolc/yourlist/playlist/domain/PlayEliminator.java
+++ src/main/java/toolc/yourlist/playlist/domain/PlayEliminator.java
@@ -1,4 +1,7 @@
 package toolc.yourlist.playlist.domain;
 
 public class PlayEliminator {
+  public void delete(ValidRequestForPlay request) {
+
+  }
 }
--- src/test/java/toolc/yourlist/playlist/domain/PlayEliminatorTest.java
+++ src/test/java/toolc/yourlist/playlist/domain/PlayEliminatorTest.java
@@ -6,10 +6,29 @@ class PlayEliminatorTest {
   @Test
   void delete() {
     var eliminator = new PlayEliminator();
-//    var request = new  ValidRequestForPlay(
+    var request = new ValidRequestForPlay(
+      Member.builder()
+        .id(1L)
+        .loginId("oh980225")
+        .password("qwer1234!")
+        .isMember(true)
+        .build(),
+      Playlist.builder()
+        .id(1L)
+        .memberId(1L)
+        .title("My List")
+        .thumbnail("panda.png")
+        .build(),
+      Play.builder()
+        .id(1L)
+        .playlistId(1L)
+        .sequence(1L)
+        .info(new PlayInfo("So Good Music", "abcd1234", "panda.png"))
+        .time(new PlayTime(1000L, 3000L))
+        .channel(new Channel("Music man", "man.png"))
+        .build()
+    );
 
-//    );
-
-//    eliminator.delete(request);
+    eliminator.delete(request);
   }
 }
