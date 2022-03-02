cd8a3ddccbf839f54e1f69c49ea5f082df58bff9
JisooPark27 <jspark9812@gmail.com>
2022-03-02 21:45:34 +0900

    feat: visitor change to member api

--- src/main/java/toolc/yourlist/member/infra/UserApi.java
+++ src/main/java/toolc/yourlist/member/infra/UserApi.java
@@ -2,11 +2,15 @@ package toolc.yourlist.member.infra;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import toolc.yourlist.common.infra.JsonResponse;
 import toolc.yourlist.member.domain.MemberAuthProvider;
 import toolc.yourlist.member.domain.TokenProvider;
 import toolc.yourlist.member.domain.VisitorAuthProvider;
+import toolc.yourlist.member.domain.VisitorToMemberChanger;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +20,7 @@ public class UserApi {
   private final VisitorAuthProvider visitorAuthProvider;
   private final MemberAuthProvider memberAuthProvider;
   private final TokenProvider tokenProvider;
+  private final VisitorToMemberChanger visitorToMemberChanger;
   private final RequestMapperFromJson requestMapperFromJson;
 
   @PostMapping("/auth/signup/visitor")
@@ -51,7 +56,7 @@ public class UserApi {
     return JsonResponse.okWithData(result.get(), "Successful member registration");
   }
 
-  @PostMapping("auth/login/member")
+  @PostMapping("/auth/login/member")
   public ResponseEntity<?> login(@RequestBody JsonMemberSignUpAndLoginRequest jsonRequest) {
     var request = requestMapperFromJson.mapper(jsonRequest);
 
@@ -62,11 +67,23 @@ public class UserApi {
     return JsonResponse.okWithData(result.get(), "Successful member login");
   }
 
-  @PostMapping("auth/reissue")
+  @PostMapping("/auth/reissue")
   public ResponseEntity<?> reissue(@RequestBody JsonTokenReissueRequest jsonRequest) {
     var request = requestMapperFromJson.mapper(jsonRequest);
 
-    var result = tokenProvider .reissue(request.accessToken(), request.refreshToken(), request.isPC());
+    var result = tokenProvider.reissue(request.accessToken(), request.refreshToken(),
+      request.isPC());
     return JsonResponse.okWithData(result, "Successful reissue token");
+  }
+
+  @PostMapping("/auth/change")
+  public ResponseEntity<?> test(@RequestBody JsonVisitorToMemberChangeRequest jsonRequest) {
+    var request = requestMapperFromJson.mapper(jsonRequest);
+
+    var result = visitorToMemberChanger.changeToMember(request);
+    if (result.isLeft()) {
+      return JsonResponse.failForBadRequest(result.getLeft());
+    }
+    return JsonResponse.okWithData(result.get(), "Successful visitor change to member");
   }
 }
