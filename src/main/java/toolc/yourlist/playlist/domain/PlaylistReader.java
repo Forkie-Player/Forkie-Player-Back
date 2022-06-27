28b8e69ab89d5e00a1bad1c107fde0bf7ae45d31
oh980225 <oh980225@gmail.com>
2022-06-27 22:42:22 +0900

    feat: Playlist조회시 => 포함된 Play갯수 추가 조회

--- src/main/java/toolc/yourlist/playlist/domain/PlaylistReader.java
+++ src/main/java/toolc/yourlist/playlist/domain/PlaylistReader.java
@@ -2,11 +2,16 @@ package toolc.yourlist.playlist.domain;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public class PlaylistReader {
   private final AllPlaylists allPlaylists;
+  private final AllPlay allPlay;
 
-  public Playlists belongsTo(User user) {
-    return allPlaylists.readAllBelongsTo(user);
+  // TODO: 1급 컬렉션으로 묶을지 고민 -> 묶는다면 Playlists 내의 로직과 유사한데 그 로직을 따로 뺄지도 고민
+  public List<PlaylistWithCount> belongsTo(User user) {
+    var playlists = allPlaylists.readAllBelongsTo(user);
+    return playlists.map(playlist -> new PlaylistWithCount(playlist, allPlay.havingCountOf(playlist.id()))).toList();
   }
 }
